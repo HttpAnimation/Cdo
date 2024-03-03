551bbe5fdef27d8f376e3d225101ffed04ad3db2
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-02 21:32:11 -0700

    adds a new import

--- cdo.c
+++ cdo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h> // Include the dirent.h header for directory-related functions
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_FILE_CONTENT_LENGTH 1000

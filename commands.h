0aee18f0d49fa1aa8929f88f2946a59085437138
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-02 21:44:56 -0700

    puts command in a h file

---
+++ commands.h
@@ -0,0 +1,11 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+// Function prototypes for command implementations
+void echo_command(const char *argument);
+void read_command(const char *argument);
+void ls_command();
+void clear_command();
+void exit_command();
+
+#endif /* COMMANDS_H */

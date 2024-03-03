8ae6247caa1fa87bf072b180b97bf8b994e83cf5
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-02 21:55:20 -0700

    new host

--- compile.bash
+++ compile.bash
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+# Compile CDo
 gcc -c main.c -o main.o
 gcc -c echo.c -o echo.o
 gcc -c read.c -o read.o
 gcc -c ls.c -o ls.o
 gcc -c clear.c -o clear.o
 gcc -c exit.c -o exit.o
+gcc -c host.c -o host.o # Add this line to compile host.c
 
-gcc main.o echo.o read.o ls.o clear.o exit.o -o CDo
+# Link object files to create the executable
+gcc main.o echo.o read.o ls.o clear.o exit.o host.o -o CDo
+
+# Optionally, you can remove the intermediate object files if you want
+# rm *.o

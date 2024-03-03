0f766c79972b36b328ae6c9025c54c66689ac9aa
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-02 21:48:06 -0700

    new compile command

--- compile.bash
+++ compile.bash
@@ -1,1 +1,8 @@
-gcc -o Cdo cdo.c
+gcc -c main.c -o main.o
+gcc -c echo.c -o echo.o
+gcc -c read.c -o read.o
+gcc -c ls.c -o ls.o
+gcc -c clear.c -o clear.o
+gcc -c exit.c -o exit.o
+
+gcc main.o echo.o read.o ls.o clear.o exit.o -o CDo
